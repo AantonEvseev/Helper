8d12ec65ac38c9a4a1e3a81384d0a6ce82fc151b
AantonEvseev <97557052+AantonEvseev@users.noreply.github.com>
2022-03-09 19:45:14 +0300

    H-4 Добавление ViewController's

---
+++ Helper/Map/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  Helper
+//
+//  Created by Anton Evseev on 09.02.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func bind() {
+        
+    }
+}
---
+++ Helper/Weather/WeatherViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherViewController.swift
+//  Helper
+//
+//  Created by Anton Evseev on 09.02.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func bind() {
+        
+    }
+}
