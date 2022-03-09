d016ead705e455705e4b3c03842122358ddbbb17
AantonEvseev <97557052+AantonEvseev@users.noreply.github.com>
2022-03-09 19:51:19 +0300

    H 4 (#2)
    * H-3 Добавить в проект SnapKit (#1)
    
    * H-4 Добавление ViewController's

---
+++ Helper/News/NewsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewsViewController.swift
+//  Helper
+//
+//  Created by Anton Evseev on 09.02.2022.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func bind() {
+        
+    }
+}
---
+++ Helper/Profile/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  Helper
+//
+//  Created by Anton Evseev on 09.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func bind() {
+        
+    }
+}
